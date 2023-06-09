63a27ce52d40528db9ab84746da4e9e6d96a842d
Arefin <ahmedarefinsajjad@gmail.com>
2023-06-09 20:44:08 +0600

    lavender: Drop xiaomi common thermal hal

--- device.mk
+++ device.mk
@@ -432,7 +432,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.xiaomi_sdm660.qti
+    android.hardware.thermal@2.0-service.xiaomi_lavender.qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
